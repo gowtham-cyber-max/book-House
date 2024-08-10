3fd2919d1be84bd96565a4d1ae9b67eb2e2909c1
gowtham-cyber-max <gowtham4262472@gmail.com>
2024-08-10 12:45:09 +0530

    docker

--- frontend/src/redux/actions/bookListAction.jsx
+++ frontend/src/redux/actions/bookListAction.jsx
@@ -2,20 +2,28 @@ import bookListService from "../services/bookListService";
 const serv = bookListService;
 
 export const postBook = (data, formData) => async (dispatch, getState) => {
-  const imgRes = await serv.postImages(formData);
-  if (imgRes.status == 201) {
-    data.imageIds = imgRes.data;
-    console.log(imgRes.data)
-  } else {
-    console.log(imgRes)
-    console.log("Error uploading images");
-    return;
+  try {
+    const imgRes = await serv.postImages(formData);
+    if (imgRes.status == 201) {
+      data.imageIds = imgRes.data;
+      console.log(imgRes.data)
+    } else {
+      console.log(imgRes)
+      console.log("Error uploading images");
+      return;
+    }
+  } catch (error) {
+    console.log(error)
   }
-  const res = await serv.postBook(data);
-  if (res.status == 200) {
-    alert("Book added successfully");
-  } else {
-    console.log("Error adding book");
+  try {
+    const res = await serv.postBook(data);
+    if (res.status == 200) {
+      alert("Book added successfully");
+    } else {
+      console.log("Error adding book");
+    }
+  } catch (error) {
+    console.log(error)
   }
 };
 
@@ -24,59 +32,73 @@ export const getNextBooks = (sort = "id", genre = "All", order = true, filter = 
     dispatch({ type: "EMPTY_LIST" });
   if (getState().books.limitReached) return;
   dispatch({ type: "NEXT_BOOKS" })
-  const res = await serv.getAllBooks(getState().books.currentPage, sort, genre, order);
-  if (res.status == 200) {
-    if (res.data.length == 0) {
-      dispatch({ type: "LIMIT_REACHED" });
-      return;
-    } else {
-      dispatch({ type: "ADD_BOOKS", payload: res.data });
-      if (res.data.length < 9) { // page limit
+  try {
+    const res = await serv.getAllBooks(getState().books.currentPage, sort, genre, order);
+    if (res.status == 200) {
+      if (res.data.length == 0) {
         dispatch({ type: "LIMIT_REACHED" });
+        return;
+      } else {
+        dispatch({ type: "ADD_BOOKS", payload: res.data });
+        if (res.data.length < 9) { // page limit
+          dispatch({ type: "LIMIT_REACHED" });
+        }
       }
     }
-  } else {
-    console.log(res);
+  } catch (error) {
+    console.log(error)
   }
 }
 
 export const getBooks =
   (attribute = "id", order = true) =>
     async (dispatch, getState) => {
-      const res = await serv.getBooks(
-        getState().books.currentPage,
-        attribute,
-        order
-      );
-      if (res.status == 200) {
-        if (res.data.length == 0) {
-          dispatch({ type: "LIMIT_REACHED" });
-          return;
-        } else {
-          dispatch({ type: "ADD_BOOKS_SUCCESS", payload: res.data });
-          if (res.data.length < 6) {
+      try {
+        const res = await serv.getBooks(
+          getState().books.currentPage,
+          attribute,
+          order
+        );
+        if (res.status == 200) {
+          if (res.data.length == 0) {
             dispatch({ type: "LIMIT_REACHED" });
+            return;
+          } else {
+            dispatch({ type: "ADD_BOOKS_SUCCESS", payload: res.data });
+            if (res.data.length < 6) {
+              dispatch({ type: "LIMIT_REACHED" });
+            }
           }
+        } else {
+          console.log(res);
         }
-      } else {
-        console.log(res);
+      } catch (error) {
+        console.log(error)
       }
     };
 
 export const searchBooks = (search) => async (dispatch, getState) => {
-  const res = await serv.searchBooks(search);
-  if (res.status == 200) {
-    dispatch({ type: "SEARCHED_BOOKS", payload: res.data });
-  } else {
-    console.log(res);
+  try {
+    const res = await serv.searchBooks(search);
+    if (res.status == 200) {
+      dispatch({ type: "SEARCHED_BOOKS", payload: res.data });
+    } else {
+      console.log(res);
+    }
+  } catch (error) {
+    console.log(error)
   }
 };
 
 export const getGenres = () => async (dispatch, getState) => {
-  const res = await serv.getGenres();
-  if (res.status == 200) {
-    dispatch({ type: "GENRES", payload: res.data });
-  } else {
-    console.log(res);
+  try {
+    const res = await serv.getGenres();
+    if (res.status == 200) {
+      dispatch({ type: "GENRES", payload: res.data });
+    } else {
+      console.log(res);
+    }
+  } catch (error) {
+    console.log(error)
   }
 };
--- frontend/src/redux/actions/userAction.jsx
+++ frontend/src/redux/actions/userAction.jsx
@@ -2,12 +2,16 @@ import userServices from "../services/userServices";
 const serv = userServices;
 
 export const signUp = (data) => async (dispatch, getState) => {
-    const res = await serv.signUp(data);
-    console.log(res)
-    if (res.status !== 201) {
-        console.log("Error adding user");
-    } else
-        dispatch({ type: "SET_USER", payload: res.data })
+    try {
+        const res = await serv.signUp(data);
+        if (res.status !== 201) {
+            console.log("Error adding user");
+        } else {
+            dispatch({ type: "SET_USER", payload: res.data })
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const signIn = (user, password) => async (dispatch, getState) => {
@@ -23,21 +27,26 @@ export const signIn = (user, password) => async (dispatch, getState) => {
             }
         }
     } catch (error) {
-        if (res.status == 406)
-            dispatch({ type: "SET_ERROR", payload: res.data })
-        else
-            console.log(res)
+        dispatch({ type: "SET_ERROR", payload: error.response.data })
     }
 }
 
 export const addToWish = (bid) => async (dispatch, getState) => {
     const uid = getState().user.user.id;
-    const res = await serv.wish(uid, bid);
+    try {
+        const res = await serv.wish(uid, bid);
+    } catch (error) {
+        console.log(error)
+    }
     dispatch({ type: "SET_USER", payload: res.data });
 }
 
 export const getWishlist = () => async (dispatch, getState) => {
     const IDs = getState().user.user.wishlist;
-    const res = await serv.getWishlistBooks(IDs);
-    dispatch({ type: "SET_WISHLIST", payload: res.data });
+    try {
+        const res = await serv.getWishlistBooks(IDs);
+        dispatch({ type: "SET_WISHLIST", payload: res.data });
+    } catch (error) {
+        console.log(error)
+    }
 }
